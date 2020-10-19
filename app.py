a3eb032a6d0b16bf219148bcd534488141a0b949
adamparker75 <adamparker75@sky.com>
2020-10-19 18:25:52 +0000

    Add register functionality

--- app.py
+++ app.py
@@ -26,13 +26,39 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 """
- building the registration functionality
- Always start by building the functionality
- for the GET method, ie return render_template
+building the registration functionality
+Always start by building the functionality
+for the GET method, ie return render_template
+We then ask if the request method is post
+And check the db to see if the username exists and
+then check it against what the user input on the form
+If there is alrady a username we redirect them back
+using the url_for to the register function.
+We then create a variable called register, which gathers
+the data on the form and acts as the else statement.
+This data is stored in a dictionary
 """
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        # If there is an existing user we flash this message
+        if existing_user:
+            flash("Username already exisits")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)  # Calls the users collection in our mongodb and uses the insert one method
+
+        # put the new user into 'Session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")     
     return render_template("register.html")
 
 
