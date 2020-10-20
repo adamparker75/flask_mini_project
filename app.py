0f72f850240d4daa96fa96f1d36ca6408bdc698b
adamparker75 <adamparker75@sky.com>
2020-10-20 18:27:00 +0000

    Add session user profile

--- app.py
+++ app.py
@@ -45,17 +45,10 @@ def register():
 
         # put the new user into 'Session cookie
         session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")     
+        flash("Registration Successful!")
+        # redirect the user to their profile page on logging in
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
-
-"""
-The only thing we need using the GET method
-is the login template
-The function checks for existing users if
-the method is POST, it checks the db for the 
-username key
-In the flash message the curly brackets are a placeholder
-"""
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -70,19 +63,31 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
-            
+
         else:
             # username doesn't exist
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
     return render_template("login.html")
-    
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
