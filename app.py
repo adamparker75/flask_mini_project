7ca64517ff26e335067952e0bc3aa8eba03f50c2
adamparker75 <adamparker75@sky.com>
2020-11-01 11:22:45 +0000

    Add manage categories template

--- app.py
+++ app.py
@@ -143,24 +143,17 @@ def edit_task(task_id):
     return render_template("edit_task.html", task=task, categories=categories)
 
 
-"""
-The below route takes the task_id as a variable
-We don't need to specify any methods as we aren't
-using any POST methods and get is the default
-We add the task_id as a parameter to the function
-Similar to how we retrieve or edit a task, we need
-to get the specific task by the ObjectId
-that matches the 'task_id' variable.
-This time we want to dlete it so we use the
-remove() method
-"""
-
-
 @app.route("/delete_task/<task_id>")
 def delete_task(task_id):
     mongo.db.tasks.remove({"_id": ObjectId(task_id)})
     flash("Task Successfully Deleted")
     return redirect(url_for("get_tasks"))
+
+
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
 
 
 if __name__ == "__main__":
